8cd694facec58c103a7300311f491d9dabbd1f45
David <dferaya@g.educaand.es>
2024-10-30 09:31:40 +0100

    Añadimos mas informacion prueba2-version_1

--- src/es/iesoretania/proyectoeclipse/datos/Prueba2.java
+++ src/es/iesoretania/proyectoeclipse/datos/Prueba2.java
@@ -4,6 +4,7 @@ public class Prueba2 {
 	public static void main(String[] args) {
 		System.out.println("Creamos prueba2");
 		System.out.println("Añadimos informacion prueba2");
+		System.out.println("Añadimos mas informacion prueba2");
 	}
 
 }
