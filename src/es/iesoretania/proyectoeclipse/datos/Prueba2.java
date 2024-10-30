47c62972762c922cc6b779195d9dda0840d8ac6b
David <dferaya@g.educaand.es>
2024-10-30 09:30:33 +0100

    Añadir información en prueba2-version_1

--- src/es/iesoretania/proyectoeclipse/datos/Prueba2.java
+++ src/es/iesoretania/proyectoeclipse/datos/Prueba2.java
@@ -3,6 +3,7 @@ package es.iesoretania.proyectoeclipse.datos;
 public class Prueba2 {
 	public static void main(String[] args) {
 		System.out.println("Creamos prueba2");
+		System.out.println("Añadimos informacion prueba2");
 	}
 
 }
